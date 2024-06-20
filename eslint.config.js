4859407bbaaf7ec70b086e0e085d7f41b629a3bb
Aleksander Mazur <19613255+OlekMazur@users.noreply.github.com>
2024-06-20 03:48:30 +0200

    .eslintrc.js -> eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+//import vue from 'eslint-plugin-vue'
+// TODO: reenable vue
+// https://github.com/vuejs/eslint-plugin-vue/issues/1291
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
+	//vue.configs.recommended,
+	{
+		rules: {
+			'indent': ['error', 'tab', {
+				'MemberExpression': 0,
+				'SwitchCase': 1,
+				'flatTernaryExpressions': true,
+			}],
+			'linebreak-style': ['error', 'unix'],
+			'quotes': ['error', 'single'],
+			'semi': ['error', 'never'],
+			'no-empty': ['error', { "allowEmptyCatch": true }],
+			/*
+			'vue/return-in-computed-property': ['error', {
+				'treatUndefinedAsUnspecified': false,
+			}],
+			'vue/no-mutating-props': ['warn'],	// TODO
+			'vue/no-reserved-component-names': ['warn'],	// TODO
+			*/
+		},
+	},
+)
